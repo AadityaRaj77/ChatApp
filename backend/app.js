f75878bc04bb3099f860e4f5f4a06590e0d20437
AadityaRaj77 <aaditya200602@gmail.com>
2025-07-13 23:50:47 +0530

    mysql connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+require('dotenv').config()
+cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 const port = 3000
 
 app.get('/', (req, res) => {
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,26 +3,26 @@ import React from "react";
 function Login() {
   return (
     <>
-      <div className="justify-items-center space-y-16 w-full h-screen">
-        <h1 className="text-3xl md:text-4xl text-center text-blue-100 mt-12 md:mt-10 font-medium text-shadow-xs text-shadow-black w-3/4 md:w-1/2">
+      <div className="justify-items-center space-y-16 h-screen w-full">
+        <h1 className="text-3xl md:text-4xl text-center text-pink-100 mt-12 md:mt-10 font-medium text-shadow-xs text-shadow-pink-50 w-3/4 md:w-1/2">
           Welcome to the Attendance Portal!
         </h1>
         <div className="justify-items-center rounded-b-xl w-3/4 md:w-1/2">
           <div className="flex w-full">
-            <button className="bg-amber-400 cursor-pointer p-2 text-white rounded-tl-xl w-1/2 border-3 border-black">
+            <button className="bg-pink-500 cursor-pointer p-2 text-white rounded-tl-xl w-1/2 border-3 border-black">
               User
             </button>
-            <button className="bg-blue-400 cursor-pointer p-2 text-white rounded-tr-xl w-1/2 border-3 border-black">
+            <button className="bg-pink-400 cursor-pointer p-2 text-white rounded-tr-xl w-1/2 border-3 border-black">
               Admin
             </button>
           </div>
-          <div className="justify-self-center justify-items-center bg-yellow-100 border-3 border-black p-8 px-12 space-y-4 rounded-b-xl w-full">
+          <div className="justify-self-center justify-items-center bg-pink-100 border-3 border-black p-8 px-12 space-y-4 rounded-b-xl w-full">
             <div className="space-y-2 w-full">
-              <p>Username</p>
+              <p>Room Name</p>
               <input
                 type="text"
-                placeholder="Enter username"
-                className="border-2 border-blue-400 p-2 rounded-xl focus:outline-none focus:border-blue-500 focus:ring-blue-500 w-full"
+                placeholder="Enter room name"
+                className="border-2 border-pink-400 p-2 rounded-xl focus:outline-none focus:border-pink-500 focus:ring-pink-500 w-full"
               ></input>
             </div>
             <div className="space-y-2 w-full">
@@ -30,11 +30,11 @@ function Login() {
               <input
                 type="text"
                 placeholder="Enter password"
-                className="p-2 border-2 border-blue-400 rounded-xl focus:outline-none focus:border-blue-500 focus:ring-blue-500 w-full"
+                className="p-2 border-2 border-pink-400 rounded-xl focus:outline-none focus:border-pink-500 focus:ring-pink-500 w-full"
               ></input>
             </div>
             <div className="w-full">
-              <button className="bg-blue-400 cursor-pointer p-2 text-white rounded-xl mt-4 w-full hover:bg-blue-600 font-medium">
+              <button className="bg-pink-400 cursor-pointer p-2 text-white rounded-xl mt-4 w-full hover:bg-pink-600 font-medium active:bg-pink-400">
                 Login
               </button>
             </div>
